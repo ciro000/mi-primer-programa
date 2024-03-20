f0a3fd08315132bf9ecdd2cce4cbb2849a466e97
ciro000 <maqqimaqui@gmail.com>
2024-03-20 20:38:14 -0300

    agregando ejercicio mi primer proyecto

---
+++ MiNombre.java
@@ -0,0 +1,7 @@
+public class MiNombre {
+    public static void main(String [] args){
+        System.out.println("Mi nombre es Ciro Díaz");
+        System.out.println("Tengo 23 años de edad.");
+        System.out.println("Mi ciudad es Lota, Chile");
+    }
+}
